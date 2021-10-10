e32ec02560c27e1acd3328d6d551bbe98c457c2e
konikasingh <konikasingh1996@gmail.com>
2021-10-10 23:22:19 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SnakeAndLadder_Day_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the snake and ladder game");
+            Console.WriteLine();
+            Console.WriteLine("Player mode:single: Starting position at 0");
+            Console.WriteLine("*Start Game*");
+            int position = 0;
+            int dice;
+            Console.ReadLine();
+        }
+    }
+}
