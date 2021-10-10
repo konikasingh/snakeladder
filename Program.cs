4dffe2ad35c854f6d2c56baa0cb300ad8a8528ac
konikasingh <konikasingh1996@gmail.com>
2021-10-10 23:31:50 +0530

    Adding 3rd

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+
 namespace SnakeAndLadder_Day_4
 {
     class Program
     {
+        //Constants 
+        public const int NO_PLAY = 0;
+        public const int LADDER = 1;
+        public const int SNAKE = 2;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the snake and ladder game.");
@@ -10,13 +16,35 @@ namespace SnakeAndLadder_Day_4
             Console.WriteLine("Player mode:single: Starting position at 0");
             Console.WriteLine("*Start Game*");
 
+
             //Variables
             int position = 0;
 
-            Random die = new Random();
-            int dice = die.Next(1, 7);
+            Random die = new Random();     //creatting random object from random class
+            Random options = new Random();
+            int dice = die.Next(1, 7);       //simulating the die throw 
             Console.WriteLine("The number on this die roll is: " + dice);
-            Console.ReadLine();
+
+            int opt = options.Next(0, 3);    //simulating the options
+
+            //options use
+            if (opt == NO_PLAY)
+            {
+                Console.WriteLine("No play: Player in same position-- " + position);
+                Console.ReadLine();
+            }
+            else if (opt == LADDER)
+            {
+                position = position + dice;
+                Console.WriteLine("Ladder! new postion-- " + position);
+                Console.ReadLine();
+            }
+            else
+            {
+                position = position - dice;
+                Console.WriteLine("Oops,Snake! new position-- " + position);
+                Console.ReadLine();
+            }
         }
     }
 }
